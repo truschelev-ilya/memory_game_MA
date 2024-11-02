8579c97cd10157c3cc1518b0ff0dca94939073c3
trushchelev-ilya <freedrex@gmail.com>
2024-11-02 13:09:24 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { init, mockTelegramEnv, parseInitData } from '@telegram-apps/sdk';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const initializeTelegramSDK = async () => {
+  try {
+    // Попытка инициализировать настоящее окружение Telegram
+    console.log("Инициализация окружения Telegram");
+    const [miniApp] = init();
+    await miniApp.ready();
+  } catch (error) {
+    // В случае ошибки инициализируем фейковое окружение
+    console.error('Ошибка при инициализации Telegram:', error);
+
+    const initDataRaw = new URLSearchParams([
+      ['user', JSON.stringify({
+        id: 99281932,
+        first_name: 'Andrew',
+        last_name: 'Rogue',
+        username: 'rogue',
+        language_code: 'en',
+        is_premium: true,
+        allows_write_to_pm: true,
+      })],
+      ['hash', '89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31'],
+      ['auth_date', '1716922846'],
+      ['start_param', 'debug'],
+      ['chat_type', 'sender'],
+      ['chat_instance', '8428209589180549439'],
+    ]).toString();
+
+    mockTelegramEnv({
+      themeParams: {
+        accentTextColor: '#6ab2f2',
+        bgColor: '#17212b',
+        buttonColor: '#5288c1',
+        buttonTextColor: '#ffffff',
+        destructiveTextColor: '#ec3942',
+        headerBgColor: '#fcb69f',
+        hintColor: '#708499',
+        linkColor: '#6ab3f3',
+        secondaryBgColor: '#232e3c',
+        sectionBgColor: '#17212b',
+        sectionHeaderTextColor: '#6ab3f3',
+        subtitleTextColor: '#708499',
+        textColor: '#f5f5f5',
+      },
+      initData: parseInitData(initDataRaw),
+      initDataRaw,
+      version: '7.2',
+      platform: 'tdesktop',
+    });
+
+    console.log('Mock Telegram environment initialized');
+  }
+};
+
+// Инициализация SDK
+initializeTelegramSDK();
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
